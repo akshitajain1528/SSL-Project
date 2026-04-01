63477d512f59255aa65bb450a4563542ad815c64
akshitajain1528 <akshitajain1528@gmail.com>
2026-04-01 18:35:37 +0000

    Modified game.py

---
+++ game.py
@@ -0,0 +1,24 @@
+class Game:
+    
+    def __init__(self):
+        self.player = 1  
+
+    def draw_board(self):
+       pass
+
+    def switch_turns(self):
+        self.player *= -1
+
+    def reset(self):
+        self.board = np.zeros(self.board_shape)
+        self.player = 1
+        self.game_over = False
+
+    def check_win(self):
+        pass
+
+    def is_empty(self, row, col):
+        return self.board[row, col] == 0
+
+    def is_full(self):
+        return np.all(self.board != 0)
